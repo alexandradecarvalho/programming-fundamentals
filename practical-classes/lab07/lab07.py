97fff5810453838333ab5d2d2badd8eaa3788f0a
Alexa <alexandracarvalho@ua.pt>
2021-07-02 12:40:11 +0100

    Added theoretical class 07 and the practical guide for practical lab 07

---
+++ practical-classes/lab07/lab07.py
@@ -0,0 +1,246 @@
+# Alexandra de Carvalho, 16 jun 2021
+
+
+# Exercise 2a) - Create the inputFloatList() function which reads a sequence of numbers inputed by the user and returns them in a list
+def inputFloatList():
+    result = []
+    user_input = input("number? ")
+    while user_input != "":
+        result += [float(user_input)]
+        user_input = input("number? ")
+    return result
+
+# Exercise 2b) - Create the countLower(lst, v) function which calculates and returns the number of lst's elements that are lower than v
+def countLower(lst, v):
+    count = 0
+    for element in lst:
+        if element < v:
+            count += 1
+    return count
+
+# Exercise 2c) - Create the minmax(lst) function which returns the maximum and the minimum values of a list, without using the min and the max functions
+def minmax(lst):
+    minimum = lst[0]
+    maximum = lst[0]
+
+    for element in lst:
+        if element < minimum:
+            minimum = element
+        elif element > maximum:
+            maximum = element
+    
+    return minimum, maximum
+
+# Exercise 2d) - Use the functions above to create a program which reads a list of numbers, determines the average between the maximum and the minimum values and that counts how many numbers are inferior to that value
+def ex2():
+    user_list = inputFloatList()
+    minimum, maximum = minmax(user_list)
+    average = (minimum + maximum) / 2
+    print("The average between the min and max numbers introduced is {}".format(average))
+    print("{} numbers introduced are lower than the average".format(countLower(user_list, average)))
+
+
+# Exercise 3a) - Complete the telToName function so that, given a phone number (and both lists), it can return the right name (or the number if it's not on the list)
+def telToName(tel, telList, nameList):
+    # your code here
+    index = 0
+    for number in telList:
+        if number == tel:
+            return nameList[index]
+        index += 1
+    return tel
+
+# Exercise 3b) - Complete the nameToTels function so that, given part of a name, it can return the list of the matching numbers for names that include the searched part
+def nameToTels(partName, telList, nameList):
+    # your code here
+    tels = []
+    index = 0
+    for name in nameList:
+        if partName in name:
+            tels += [telList[index]]
+        index +=1
+    if tels:
+        return tels
+    return partName 
+
+# Exercise 3c) - Run the program to test these functions
+def ex3():
+    telList = ['975318642', '234000111', '777888333', '911911911']
+    nameList = ['Angelina', 'Brad',      'Claudia',   'Bruna']
+
+    # Test telToName:
+    tel = input("Tel number? ")
+    print( telToName(tel, telList, nameList) )
+    print( telToName('234000111', telList, nameList) == "Brad" )
+    print( telToName('222333444', telList, nameList) == "222333444" )
+
+    # Test nameToTels:
+    name = input("Name? ")
+    print( nameToTels(name, telList, nameList) )
+    print( nameToTels('Clau', telList, nameList) == ['777888333'] )
+    print( nameToTels('Br', telList, nameList) == ['234000111', '911911911'] )
+
+
+# Exercise 4 - Write a function that, given a list of football teams, generates a list of all matches that can be done
+def footbal_matches(lst):
+    matches = []
+    for team1 in lst:
+        for team2 in lst[-1::-1]:
+            if team1 != team2:
+                matches.append(tuple([team1, team2]))
+    return matches
+
+def ex4():
+    teams_list = ['FCP', 'Belenenses', 'SLB', 'Boavista', 'Olhanense', 'SCP']
+    print(footbal_matches(teams_list))
+
+
+# Exercise 5 - Write a function that counts how many digits appear in a given string: countDigits("23 mil 456") should return 5
+def countDigits(text):
+    count = 0
+    for letter in text:
+        if letter.isdigit():
+            count += 1
+    return count
+
+def ex5():
+    text = input("Insert sentence: ")
+    print("\"{}\" has {} digits".format(text, countDigits(text)))
+
+
+# Exercise 6 - Write a function that, given a name, creates a short version, formed only by the capital letters: shorten("Universidade de Aveiro") = "UA"
+def shorten(text):
+    result = ""
+    for letter in text:
+        if letter.isupper():
+            result += letter
+    return result
+        
+def ex6():
+    text = input("Insert sentence: ")
+    print(shorten(text))
+
+
+# Exercise 7 - Write a ispalindrome(s) function that returns a boolean value indicating whether or not the string is a palindrome
+def isPalindrome(s):
+    while s:
+        if s[0] != s[-1]:
+            return False
+        else:
+            return isPalindrome(s[1:-1])
+    return True
+
+def ex7():
+    s = (input("Insert sentence: "))
+    if isPalindrome(s):
+        print("\"{}\" is palindrome!".format(s))
+    else:
+        print("\"{}\" is not a palindrome :/".format(s))
+
+
+# Exercise 8a) - Write a function that, given a string, returns another one, made up by all characters of even positions followed by all characters of odd positions of the first string 
+def evenThenOdd(s):
+    return s[::2] + s[1::2]
+    
+# Exercise 8b) - Write a function that, given a string s, returns a similar string but without duplicate adjacent characters
+def noAdjacent(s):
+    result = s[0]
+    index = 1
+    while index < len(s):
+        if s[index] != s[index-1]:
+            result += s[index]
+        index += 1
+    return result
+
+#Exercise 8c) - Write a function that, given a non-negative integer n, returns a list with 1,2,2,3,3,3,4,4,4,... until n repeated n times
+def repeatNTimes(n):
+    result = []
+    current_int = 1
+    while current_int <= n:
+        count = 0
+        while count < current_int:
+            result.append(current_int)
+            count += 1
+        current_int += 1
+    return result
+
+# Exercise 8d) - Write a function that, given a not-empty list of values, returns the index of the first occurrence of the highest value, without using the functions max, find or index
+def firstHighestValue(input_list):
+    highest = input_list[0]
+    index = 0
+    result = 0
+    for elem in input_list:
+        if elem > highest:
+            highest = elem
+            result = index
+        index += 1
+    return result
+
+def ex8():
+    user_string = input("string? ")
+    print("String with even positions then odd positions: \"{}\" \n".format(evenThenOdd(user_string)))
+    print("String with no adjacent equal characters: \"{}\" \n".format(noAdjacent(user_string)))
+    
+    user_input = int(input("number? "))
+    print("List of repeatNTimes: {} \n".format(repeatNTimes(user_input)))
+    
+    user_list = []
+    elem = input("list element? ")
+    while elem != "":
+        user_list += [int(elem)]
+        elem = input("list element? ")
+    print("Highest value is at position {} \n".format(firstHighestValue(user_list)))
+
+##################MAIN#####################
+
+def print_menu():
+    print(30 * "-" , "MENU" , 30 * "-")
+    print("2. Exercise 2")
+    print("3. Exercise 3")
+    print("4. Exercise 4")
+    print("5. Exercise 5")
+    print("6. Exercise 6")
+    print("7. Exercise 7")
+    print("8. Exercise 8")
+    print("0. Exit")
+    print(67 * "-")
+
+def main():
+    loop = True
+
+    while loop:
+        print_menu()
+        try:
+            choice = int(input("Enter your choice [5-9] or 0 to quit: "))
+        except:
+            choice = 222
+        
+        if choice==2:     
+            print("Exercise 2: \n")
+            ex2()
+        elif choice==3:     
+            print("Exercise 3: \n")
+            ex3()
+        elif choice==4:
+            print("Exercise 4: \n")
+            ex4()
+        elif choice==5:     
+            print("Exercise 5: \n")
+            ex5()
+        elif choice==6:
+            print("Exercise 6: \n")
+            ex6()
+        elif choice==7:
+            print("Exercise 7: \n")
+            ex7()
+        elif choice==8:
+            print("Exercise 8: \n")
+            ex8()
+        elif choice==0:
+            print("Goodbye")
+            loop=False # This will make the while loop to end as not value of loop is set to False
+        else:
+            # Any integer inputs other than values 1-5 we print an error message
+            print("Wrong option selection. Enter any key to try again..")
+
+main()
---
+++ theoretical-classes/class07/class07.md
@@ -0,0 +1,288 @@
+## Programming Fundamentals, Class 07
+
+###### this is an adaptation of the materials provided by Professor João Manuel de Oliveira e Silva Rodrigues
+
+
+
+### Summary
+
+* Files
+* Command Line Arguments
+* Exceptions and Assertions
+
+
+
+### Text Files
+
+* Most of the programs we have seen so far are transient in the sense that they run for a short time, take input and produce output, but when they end, everything disappears
+
+* One of the simplest ways for programs to maintain their data is by reading and writing text files
+
+* A text file is a sequence of characters stored on a persistent medium like a hard drive, flash memory, or CD-ROM
+
+* Characters are encoded in bytes according to a standard coding table such as UTF-8 or ASCII, for instance
+
+  
+
+### Opening and Closing Files
+
+* We must prepare a file before reading and writing. This is called **opening** the file
+* The built-in function `open` takes the name of the file and returns a file object that we can use to access it
+
+```python
+fileobj = open(file_name, 'r')	# open for reading
+fileobj = open(file_name, 'w')	# open for writing
+```
+
+
+
+* More modes: 'r', 'w', 'a', 'r+', 'w+', 'a+', 'rb'...
+* After using the file, remember to **close** it: `fileobj.close()`
+* The `with` statement will **automatically close** files
+
+```python
+with open(file_name, mode) as fileobj:
+	statement to read/write fileobj
+# fileobj.close not required!
+```
+
+
+
+### Text *versus* Binary Mode
+
+* Normally, files are opened in ***text mode***. This means:
+  * You read/write *strings* of characters (type `str`)
+  * Characters are *encoded*: one character may use several bytes  (the encoding may be specified with the encoding = `optional` argument)
+  * Platform-specific *line endings* are converted to/from `\n`
+* For files that don't contain text, you should use '`rb`' or '`wb`' to open in ***binary mode***. This means:
+  * You read/write strings of *bytes* (type `bytes`, not `str`)
+  * No conversions occur
+
+
+
+### Reading a File
+
+* We can use a `for` loop to read a file *line by line*
+
+```python
+fin = open('words.txt')
+for line in fin:	# for each line from the file
+	print(line)		# do something with it
+fin.close()
+```
+
+
+
+* Another way is using the `readline` method 
+
+```python
+while True:
+    line= fin.readline()	# returns line to the end
+	if line == "": break	# empty means end-of-file
+	print(line)		
+```
+
+
+
+* We can also read the entire file as string: `text = fin.read()	# read as much as possible (up to EOF)`
+* Or read at most N characters: str = `fin.read(10) # read upto 10 chars (empty means EOF)`
+
+
+
+### Moving the File Cursor
+
+* We generally read and write sequentially, from start to end
+
+* But sometimes we need to "jump" around
+
+* The `tell()` method tells you the current position within the file
+
+* The `seek(offset)` method changes the current file position to `offset` bytes from the start (an optional argument can specify a different reference point)
+
+```
+a0 = f.readline()	# read a line
+pos ⁼ f.tell()		# store position
+a1 = f.readline()	# read second line
+f.seek(pos)			# return the stored position
+a2 = f.readline()	# read second line again (a2 == a1)
+```
+
+
+
+### Write to a File
+
+* To write to a file, open it with mode '`w`' (or '`a`'): `fout = open('output.txt','w')`
+* Opening it in '`w`' mode creates a **new file** or *truncates* an existing one, *i.e.* it **deletes** the old data and starts from scratch
+* The `write` method puts data into the file:
+
+```python
+line1 = 'To be or not to be, \n'
+fout.write(line1)
+```
+
+
+
+* Again, the `file` object keeps track of where it is, so if you call `write` again, it adds the new data to the end: 
+
+```python
+line2 = 'That is the question.\n'
+fout.write(line2)
+```
+
+
+
+* The argument of write has to be a string, so we have to convert other types of values:
+
+```python
+x = 0.75
+fout.write('X: ' + str(x))
+```
+
+
+
+* Or use the string format method: `fout.write('{s:} costs {:.2f}€.'.format('coffee',x))`
+* You may also use `print` with the `file=` argument:
+
+```python
+print('X: ',x, file=fout)
+print('{:s} costs {:.2f}€.'.format('coffe',x), file=fout)
+```
+
+
+
+* When you are done writing, remember to close the file! `fout.close() # OR use the with statement`
+
+
+
+### Filenames and Paths
+
+* The `os` module provides functions for working with files and directories ("`os`" stands for "operating system")
+* `os.getcwd()` returns the name of the current directory
+* A string that identifies a file is called a ***path***
+* An **absolute path** starts with `/` (the topmost directory)
+* A **relative path** starts from the current directory
+* To find an absolute path to a file, you can use: `os.path.abspath(path)`
+* There are functions to check existence and type of files:
+  * `os.path.exists(f)` checks whether a file exists
+  * `os.path.dir(f)` checks whether a filename is a directory
+  * `os.path.isfile(f)` checks whether it's a regular file
+* And a function to get the contents of a directory:
+  * `os.listdir` returns a list of the files (and other directories) in the given directory
+  * backward from the end
+
+
+
+### Command Line Arguments
+
+* The Python `sys` module provides access to any command-line arguments via the `sys.argv`:
+  * `sys.argv` is the list of command-line arguments
+  * `len(sys.argv)` is the number of command-line arguments
+  * `sys.argv[0]` is the program (script) name
+
+```python
+import sys
+print('Number of args: ', len(sys.argv), ' arguments.')
+print('Argument list: ', sys.argv)
+```
+
+
+
+* Running the above script as `python3 test.py arg1 arg2 arg3` produces:
+
+```python
+Number of args: 4 arguments.
+Argument list: ['test.py','arg1','arg2','arg3']
+```
+
+
+
+### Exceptions
+
+* Python provides an important feature to handle any unexpected events in your program: **exceptions**
+* You've seen exceptions before:
+
+```python
+int("one")	#-> ValueError: invalid literal for int()
+open("foo")	#-> FileNotFoundError: No such file...
+```
+
+
+
+* When Python encounters a situation that it cannot cope with, it ***raises*** an exception
+* That **interrupts** the normal flow of the execution: the current function is interrupted, then the one that called it, etc., until the main program itself is interrupted
+* Information about the event is transmitted all the way through in an *exception object*
+
+
+
+### Handling Exceptions
+
+* You can intercept selected exceptions and recover normal execution with the `try` statement:
+
+```python
+try:
+    fh = open('testfile', 'r')
+    fh.read()
+except IOError:
+    print('Error: can\'t find file or read data')
+else:
+    print('Written content in the file successfully')
+    fh.close()
+```
+
+
+
+* The `except` statement can also be used with no exceptions or with more than one
+
+
+
+### Exception Information
+
+* An exception can have an *argument*, which is a value that gives additional information about the problem:
+
+```python
+def temp_convert(var):
+    try:
+        return int(var)
+    except ValueError as x:
+        print('Not numeric\n', x)
+
+temp_convert("xyz")
+```
+
+
+
+### Raising Exceptions
+
+* We can raise exceptions (of any type) by using the raise statement
+
+  ```python
+  def functionName(level):
+  	if level <1:
+  		raise Exception(level)
+      # code here is not executed if we raise the exception
+      return level
+  
+  try:
+  	v = functionName(-10)
+  	print("level = ", v)
+  except Exception as e:
+  	print("error in level argument", e.args[0])
+  ```
+
+
+
+### Assertions
+
+* An **assertion** is a condition that we know (or require) to be true at some point in a program
+* Use the `assert` statement for checking assertions
+* It evaluates the condition and, if false, raises an exception
+* We can turn off assertion checking when we are done with testing of the program (call Python with -o flag)
+* We can place assertions at the start of a function to check for valid input, or after a function call to check for valid output:
+
+```python
+def KelvinToFahrenheit(temperature):
+	assert temperature >= 0, "Colder than absolute zero!"
+	return ((temperature-273)*1.8)+32
+
+print(KelvinToFahrenheit(-5))	#-> AssertionError: Colder than absolute zero!
+```
