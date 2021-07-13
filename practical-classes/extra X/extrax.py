43f077c0ed099a71fd0dbfbadc754c5d300b2609
Alexa <alexandracarvalho@ua.pt>
2021-07-13 11:54:12 +0100

    Developed option to register a new call

--- practical-classes/extra X/extrax.py
+++ practical-classes/extra X/extrax.py
@@ -2,14 +2,13 @@
 
 
 # Exercise 1a) - The program must present a menu and process each chosen option
-def print_menu():
+def print_menu_ex1():
     print(30 * "-" , "MENU" , 30 * "-")
     print("1) Register call")
     print("2) Read file")
     print("3) List clients")
     print("4) Bill")
     print("5) Exit")
-    print("Option? ")
 
 # Exercise 1b) - Write a function to validate a phone number - a string with at least 3 digits (0-9), optionally with a '+' symbol in the beginning
 def isValidPhone(phone_number):
@@ -26,31 +25,48 @@ def isValidPhone(phone_number):
     else:
         return True
 
-# Exercise 1c) - 
+# Exercise 1c) - Add the option to register a new call (from the keyboard) using the previous function in order to validate the origin and destiny phone numbers
+def registerCall():
+    origin_phone_number = ""
+    while not origin_phone_number or not isValidPhone(origin_phone_number):
+        origin_phone_number = input("Origin Phone Number? ")
+    
+    destiny_phone_number = ""
+    while not destiny_phone_number or not isValidPhone(destiny_phone_number):
+        destiny_phone_number = input("Destiny Phone Number? ")
+    
+    while True:
+        duration = input("Duration (s)? ")
+        try:
+            duration = int(duration)
+        except ValueError:
+            print("ERROR: Invalid duration, please insert a number")
+        else:
+            break
 
 def ex1():
-    loop = True
-    while loop:
-        print_menu()
+    register_loop = True
+    while register_loop:
+        print_menu_ex1()
         try:
-            choice = int(input("Enter your choice [5-9] or 0 to quit: "))
+            option = int(input("Option? "))
         except:
-            choice = 222    # Invalid option
+            option = 222    # Invalid option
 
-    if choice==1:
-        print("Register Call: \n")
-    elif choice==2:     
-        print("Read File: \n")
-    elif choice==3:     
-        print("List Clients: \n")
-    elif choice==4:     
-        print("Bill: \n")
-    elif choice==5:     
-        print("Goodbye")
-        loop=False # This will make the while loop to end as not value of loop is set to False
-    else:
-        # Any integer inputs other than values 1-5 we print an error message
-        print("Wrong option selection. Enter any key to try again..")
+        if option==1:
+            registerCall()
+        elif option==2:     
+            print("Read File: \n")
+        elif option==3:     
+            print("List Clients: \n")
+        elif option==4:     
+            print("Bill: \n")
+        elif option==5:     
+            print("Goodbye")
+            register_loop=False # This will make the while loop to end as not value of loop is set to False
+        else:
+            # Any integer inputs other than values 1-5 we print an error message
+            print("Wrong option selection. Enter any key to try again..")
 
 
 ##################MAIN#####################
@@ -58,8 +74,6 @@ def ex1():
 def print_menu():
     print(30 * "-" , "MENU" , 30 * "-")
     print("1. Exercise 1")
-    print("2. Exercise 2")
-    print("3. Exercise 3")
     print("0. Exit")
     print(67 * "-")
 
@@ -74,14 +88,8 @@ def main():
             choice = 222    # Invalid option
         
         if choice==1:
-            print("Exercise 2: \n")
+            print("Exercise 1: \n")
             ex1()
-        elif choice==2:     
-            print("Exercise 2: \n")
-            ex2()
-        elif choice==3:     
-            print("Exercise 3: \n")
-            print(ex3())
         elif choice==0:
             print("Goodbye")
             loop=False # This will make the while loop to end as not value of loop is set to False
