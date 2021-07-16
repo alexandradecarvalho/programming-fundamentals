5b5c3792fd4f7bde8775e8c1c895687ecfddac7c
Alexa <alexandracarvalho@ua.pt>
2021-07-16 18:28:28 +0100

    Altered the printStock function from Ex 01 to print a  formatted table, including a new table of the valuation percentage

--- practical-classes/extraY/extray.py
+++ practical-classes/extraY/extray.py
@@ -1,126 +1,55 @@
-# Alexandra de Carvalho, 13 jul 2021
+# Alexandra de Carvalho, 16 jul 2021
 
 
-import time
-import json
-
-
-# Exercise 1a) - The program must present a menu and process each chosen option
-def print_menu_ex1():
-    print(30 * "-" , "MENU" , 30 * "-")
-    print("1) Register call")
-    print("2) Read file")
-    print("3) List clients")
-    print("4) Bill")
-    print("0) Exit")
+# Exercise 1a) - Alter the printStocks function to show the table with aligned and formatted columns, with an extra column with the valuation of the stock, in percentage
+def printStocks(stocks):
+    for stock in stocks:
+        print("{:<7s} {:<15s} {:>7.2f} {:>8.2f} {:>10d} {:>7.1f}%".format(stock[0],stock[1],stock[2],stock[3],stock[4], (stock[3]-stock[2])*100/stock[2]))
 
 # Exercise 1b) - Write a function to validate a phone number - a string with at least 3 digits (0-9), optionally with a '+' symbol in the beginning
 def isValidPhone(phone_number):
-    if phone_number[0] == '+':
-        phone_number = phone_number[1:]
-    
-    if len(phone_number) < 3:
-        return False
-    
-    try:
-        phone_number = int(phone_number)
-    except ValueError:
-        return False
-    else:
-        return True
+    pass
 
 # Exercise 1c) - Add the option to register a new call (from the keyboard) using the previous function in order to validate the origin and destiny phone numbers
 def registerCall():
-    origin_phone_number = ""
-    while not origin_phone_number or not isValidPhone(origin_phone_number):
-        origin_phone_number = input("Origin Phone Number? ")
-    
-    destiny_phone_number = ""
-    while not destiny_phone_number or not isValidPhone(destiny_phone_number):
-        destiny_phone_number = input("Destiny Phone Number? ")
-    
-    while True:
-        duration = input("Duration (s)? ")
-        try:
-            duration = int(duration)
-        except ValueError:
-            print("ERROR: Invalid duration, please insert a number")
-        else:
-            break
+    pass
 
 # Exercise 1d) - Develop an option to read a file of calls that, in each call, is registered 3 "words" separated by white spaces: the origin number, the destiny number and the duration in seconds
 def read_file(fname):
-    calls = []
-    try:
-        f = open(fname,'r')
-    except IOError:
-        print("ERROR while opening file "+fname)
-    else:
-        for line in f:
-            call = tuple(line.replace("\n","").split("\t"))
-            calls += [call]
-        f.close()
-    return calls
+    pass
 
-# Exercise 1e) - The "List Clients" option should show an ordered list of the clients who made calls, without repetitions
-def list_clients(reg):
-    origin_numbers = set([call[0] for call in reg])
-    print(sorted(list(origin_numbers)))
-    
-# Exercise 1f) - Develop an option to produce a detailed invoice, asking the client number and listing all the calls made, their duration and cost, as well as the total cost
-def billing():
-    client = input("Please insert your number: ")
-    while not isValidPhone(client):
-        print("ERROR: Invalid phone number")
-        client = input("Please insert your number: ")
-    
-    reg = read_file('chamadas1.txt')
-    total_price = 0
-    print("Invoice for client ",client)
-    print("{:<15s}   {:>8s}  {:>8s}".format("Destiny","Duration","Cost"))
-    
-    for call in reg:
-        if call[0] == client:
-            if call[1][0] == "2":
-                tarif = 0.02
-            elif call[1][0] == "+":
-                tarif = 0.8
-            elif call[1][:2] == client[:2]:
-                tarif = 0.04
-            else:
-                tarif = 0.1
-            
-            duration = call[2]
-            cost = tarif / 60 * int(duration)
-            total_price += cost
-            print("{:<15s}   {:>8s}  {:>8.2f}".format(call[1],duration,cost))
-    
-    print("{:<15s}   {:>8s}  {:>8.2f}".format("","Total:",total_price))
-            
 def ex1():
-    register_loop = True
-    while register_loop:
-        print_menu_ex1()
-        try:
-            option = int(input("Option? "))
-        except:
-            option = 222    # Invalid option
+    stocks = [
+    ('INTC', 'London', 34.249, 34.451, 1792860),
+    ('TSLA', 'London', 221.33, 229.63, 398520),
+    ('EA', 'Paris', 72.63, 68.98, 1189510),
+    ('INTC', 'Tokyo', 33.22001, 34.28999, 4509110),
+    ('TSLA', 'Paris', 217.35, 217.75, 252500),
+    ('ATML', 'Frankfurt', 8.23, 8.36, 810440),
+    ]
 
-        if option==1:
-            registerCall()
-        elif option==2:     
-            print(read_file('chamadas1.txt'))
-            print(read_file('chamadas2.txt'))
-        elif option==3:     
-            list_clients(read_file('chamadas1.txt'))
-        elif option==4:     
-            billing()
-        elif option==0:     
-            print("Goodbye")
-            register_loop=False # This will make the while loop to end as not value of loop is set to False
-        else:
-            # Any integer inputs other than values 1-5 we print an error message
-            print("Wrong option selection. Enter any key to try again..")
+    print("\na)")
+    printStocks(stocks)
+
+    print("\nb)")
+    stocks2 = sorted(stocks  )
+    printStocks(stocks2)
+
+    print("\nc)")
+    stocks3 = [ ]
+    
+    printStocks(stocks3)
+
+    print("\nd)")
+    #stocks4 = load("stocks.txt")
+    #printStocks(stocks4)
+    # As condições seguintes devem ser verdadeiras
+    #assert type(stocks4)==list
+    #assert type(stocks4[0])==tuple
+    #assert len(stocks4[0])==5
+    #assert type(stocks4[0][2])==float
+    #assert type(stocks4[0][4])==int
+    print("FIM")
 
 
 # Exercise 2a) - Create an interface with the player that registers the bets to each tournament game: the user introduces the tournament number and then the bet to each game, which is asked by the program, that then validates the tournament's number (it has to be in the Jornadas.txt file) and each bet (only 1,X,2 allowed) and stores a file with the name of the tournament with all bets made, one per line
